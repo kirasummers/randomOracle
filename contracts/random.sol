5df6258449d5276c17e14b55e6b0ecc65be3712c
kirasummers <65813267+kirasummers@users.noreply.github.com>
2020-06-02 09:03:36 +0100

    update

--- contracts/random.sol
+++ contracts/random.sol
@@ -1,6 +1,6 @@
 pragma solidity >= 0.5.0 < 0.6.0;
 
-import "github.com/provable-things/ethereum-api/provableAPI.sol";
+import "./provableAPI_0.5.sol";
 
 contract RandomExample is usingProvable {
 
